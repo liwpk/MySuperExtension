114e24f6095fe947d52e1ddc1c17a50b87da8e15
David Li <liwpk@163.com>
2026-01-19 16:38:40 +0800

    update

---
+++ Sources/MySuperExtension/Bundle+Ext.swift
@@ -0,0 +1,29 @@
+//
+//  Bundle+Ext.swift
+//  MySuperExtension
+//
+//  Created by David Li on 2026/1/14.
+//
+
+import UIKit
+
+extension Bundle {
+    /// 版本号
+    /// returns the value accessed with the key `CFBundleShortVersionString`.
+    var versionNumber: String {
+        guard let infoDictionary = Bundle.main.infoDictionary else { return "nil" }
+        guard let versionNumber = infoDictionary["CFBundleShortVersionString"] as? String else { return "nil" }
+        return versionNumber
+    }
+    
+    var buildString: String {
+        Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? ""
+    }
+    
+    /// Retrieves the `infoDictionary` dictionary inside Bundle, and retrieves
+    /// all the values inside it
+    var getInfoDictionary: [String: Any] {
+        guard let infoDictionary = Bundle.main.infoDictionary else { return [:] }
+        return infoDictionary
+    }
+}
